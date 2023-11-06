d978641144bbb056b85fbe7106ad343bc0d1480d
Nico Kempe <accounts@nicokempe.de>
2023-11-06 22:26:13 +0100

    Create command index

---
+++ src/commands/index.ts
@@ -0,0 +1,8 @@
+// src/commands/index.ts
+import * as hello_there from "./hello_there";
+import * as joke from "./joke";  // Import the new command
+
+export const commands = {
+    hello_there,
+    joke,
+};
