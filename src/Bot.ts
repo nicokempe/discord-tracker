7060652979512e01cbe66131f4618326bc0101c5
Nico Kempe <accounts@nicokempe.de>
2024-05-26 17:58:51 +0200

    feat(activity): add activity broadcasting to public channel

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,12 +1,14 @@
-import {ActivityType, Client, TextChannel} from 'discord.js';
+import { ActivityType, Client, MessageCreateOptions, MessagePayload, TextChannel} from 'discord.js';
 import * as interactionCreate from "./events/interactionCreate";
-import { ready } from './events/ready';
+import {ready} from './events/ready';
 import 'dotenv/config';
 import * as process from "process";
 
 const token: string = process.env.DISCORD_TOKEN as string;
 const userId: string = process.env.USER_ID as string;
 const channelId: string = process.env.CHANNEL_ID as string;
+const mainUserId: string = process.env.MAIN_USER_ID as string;
+const fanClubChannelId: string = process.env.FAN_CLUB_CHANNEL_ID as string;
 
 type Status = {
     name: string;
@@ -14,10 +16,10 @@ type Status = {
 };
 
 const statuses: Status[] = [
-    { name: 'automations', type: 'Playing' },
-    { name: 'code', type: 'Playing' },
-    { name: 'networking', type: 'Playing' },
-    { name: 'consulting', type: 'Playing' },
+    {name: 'automations', type: 'Playing'},
+    {name: 'code', type: 'Playing'},
+    {name: 'networking', type: 'Playing'},
+    {name: 'consulting', type: 'Playing'},
 ];
 
 const client = new Client({
@@ -32,7 +34,7 @@ client.on('ready', () => {
     const updateStatus = () => {
         const randomStatus = statuses[Math.floor(Math.random() * statuses.length)];
         try {
-            client.user?.setActivity(randomStatus.name, { type: ActivityType[randomStatus.type] });
+            client.user?.setActivity(randomStatus.name, {type: ActivityType[randomStatus.type]});
             console.log(`Updated status to: ${randomStatus.name}`);
         } catch (error) {
             console.error(`Failed to update status: ${(error as Error).message}`);
@@ -44,16 +46,16 @@ client.on('ready', () => {
 });
 
 client.on('presenceUpdate', (oldPresence, newPresence) => {
-    if (newPresence.userId === userId) {
+    const timestamp = new Intl.DateTimeFormat('de-DE', {
+        year: 'numeric', month: '2-digit', day: '2-digit',
+        hour: '2-digit', minute: '2-digit', second: '2-digit'
+    }).format(new Date());
+
+    const createPresenceMessage = (newPresence: any, channelId: string, prefix: string, isFanClub: boolean) => {
         const activities = newPresence.activities;
         const status = newPresence.status;
         const clientStatus = newPresence.clientStatus;
-        const timestamp = new Intl.DateTimeFormat('de-DE', {
-            year: 'numeric', month: '2-digit', day: '2-digit',
-            hour: '2-digit', minute: '2-digit', second: '2-digit'
-        }).format(new Date());
 
-        // Preparing the client status part of the message
         let clientStatusMessage = [];
         if (clientStatus) {
             if (clientStatus.desktop) clientStatusMessage.push(`${clientStatus.desktop} on Desktop :desktop:`);
@@ -62,10 +64,14 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
         }
         const clientStatusString = clientStatusMessage.join(', ');
 
-        // Combining the status and client status in one line with the title
-        let message = `### :boom: Presence Update\n[${timestamp}] Your new activity status is **${status}**. (${clientStatusString})\n`;
+        let message: string | MessagePayload | MessageCreateOptions;
+        if (isFanClub) {
+            message = `### ${prefix} ${newPresence.user?.tag}'s activity update\n[${timestamp}] The new activity status is **${status}**. (${clientStatusString})\n`;
+        } else {
+            message = `### ${prefix} Your new activity status is **${status}**. (${clientStatusString})\n`;
+        }
 
-        activities.forEach(activity => {
+        activities.forEach((activity: { type: any; name: any; }) => {
             let type;
             switch (activity.type) {
                 case 0: type = 'Playing'; break;
@@ -75,24 +81,32 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
                 case 4: type = 'Custom Status'; break;
                 default: type = 'Unknown'; break;
             }
-            message += `[${timestamp}] *${type}* **${activity.name}**\n`;
+            if (isFanClub) {
+                message += `[${timestamp}] *${type}* **${activity.name}**\n`;
+            } else {
+                message += `[${timestamp}] *${type}* **${activity.name}**\n`;
+            }
         });
 
-        // Actual message
         const channel = client.channels.cache.get(channelId) as TextChannel;
-
-        // Check if the channel is a text channel and then send the message
         if (channel) {
-            channel.send(message)
-                .catch(console.error); // Catch and log any errors
+            channel.send(message).catch(console.error);
         } else {
             console.error(`[${timestamp}] Could not find the channel.`);
         }
+    };
+
+    if (newPresence.userId === userId) {
+        createPresenceMessage(newPresence, channelId, ':boom:', false);
+    }
+
+    if (newPresence.userId === mainUserId) {
+        createPresenceMessage(newPresence, fanClubChannelId, ':star:', true);
     }
 });
 
 client.on('presenceUpdate', (oldPresence, newPresence) => {
-    if (newPresence.userId === userId) {
+    if (newPresence.userId === userId || newPresence.userId === mainUserId) {
         const timestamp = new Intl.DateTimeFormat('de-DE', {
             year: 'numeric', month: '2-digit', day: '2-digit',
             hour: '2-digit', minute: '2-digit', second: '2-digit'
@@ -104,11 +118,10 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
                 const trackArtist = activity.state;
                 const trackAlbum = activity.assets?.largeText;
 
-                // Construct the message with the timestamp
                 const spotifyMessage = `[${timestamp}] :musical_note: Listening to "**${trackName}**" by "*${trackArtist}*" on the album "*${trackAlbum}*".`;
 
-                // Send to a specific channel
-                const channel = client.channels.cache.get(channelId) as TextChannel;
+                const targetChannelId = newPresence.userId === userId ? channelId : fanClubChannelId;
+                const channel = client.channels.cache.get(targetChannelId) as TextChannel;
                 if (channel) channel.send(spotifyMessage).catch(console.error);
             }
         });
